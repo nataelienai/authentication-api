ea95ed486eee92bacbf6836fee3e89b5f84469ca
Natã Elienai <nata.elienai@gmail.com>
2023-02-27 16:54:13 -0300

    fix: set name property of email already exists error

--- src/application/errors/email-already-exists-error.ts
+++ src/application/errors/email-already-exists-error.ts
@@ -1,5 +1,6 @@
 export class EmailAlreadyExistsError extends Error {
   constructor(email: string) {
     super(`The email '${email}' is already registered`);
+    this.name = 'EmailAlreadyExistsError';
   }
 }
