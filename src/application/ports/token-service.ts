a8e5ee700f4280bf3137ddfc362c2aca1137e61f
Natã Elienai <nata.elienai@gmail.com>
2023-02-27 17:36:23 -0300

    feat: add decode to token service contract

--- src/application/ports/token-service.ts
+++ src/application/ports/token-service.ts
@@ -1,3 +1,7 @@
+import { Either } from '@/shared/either';
+import { InvalidTokenError } from '../errors/invalid-token-error';
+
 export interface TokenService {
   encode(userId: string): Promise<string>;
+  decode(token: string): Promise<Either<InvalidTokenError, string>>;
 }
