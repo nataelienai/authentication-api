0eac8c73611d4531998245db6f97eb35cbfd9772
Natã Elienai <nata.elienai@gmail.com>
2023-02-27 16:49:42 -0300

    feat: add findByEmail to user repository contract

--- src/application/ports/user-repository.ts
+++ src/application/ports/user-repository.ts
@@ -3,4 +3,5 @@ import { User } from '@/domain/user';
 export interface UserRepository {
   create(user: User): Promise<void>;
   existsByEmail(email: string): Promise<boolean>;
+  findByEmail(email: string): Promise<User | null>;
 }
