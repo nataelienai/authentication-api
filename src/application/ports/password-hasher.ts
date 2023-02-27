0c7f84a6b4b3a74320fcea2f2f8e2cf744a40d60
Natã Elienai <nata.elienai@gmail.com>
2023-02-27 16:50:18 -0300

    feat: add compare to password hasher contract

--- src/application/ports/password-hasher.ts
+++ src/application/ports/password-hasher.ts
@@ -3,4 +3,8 @@ import { Password } from '@/domain/password';
 
 export interface PasswordHasher {
   hash(password: Password): Promise<HashedPassword>;
+  compare(
+    plainTextPassword: string,
+    hashedPassword: HashedPassword,
+  ): Promise<boolean>;
 }
