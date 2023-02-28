2c08e614fac160c10f82946bd171a8fbfad2755c
Natã Elienai <nata.elienai@gmail.com>
2023-02-28 19:39:34 -0300

    feat: add existsById and deleteById to user repository contract

--- src/application/ports/user-repository.ts
+++ src/application/ports/user-repository.ts
@@ -3,7 +3,9 @@ import { User } from '@/domain/user';
 export interface UserRepository {
   create(user: User): Promise<void>;
   existsByEmail(email: string): Promise<boolean>;
+  existsById(userId: string): Promise<boolean>;
   findByEmail(email: string): Promise<User | null>;
   findById(userId: string): Promise<User | null>;
   update(user: User): Promise<void>;
+  deleteById(userId: string): Promise<void>;
 }
