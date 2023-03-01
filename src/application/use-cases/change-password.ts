9a336f036ec9ef9331e128c29bd63d6854f0012d
Natã Elienai <nata.elienai@gmail.com>
2023-03-01 18:42:32 -0300

    fix: add blacklisted token check to change password use case

--- src/application/use-cases/change-password.ts
+++ src/application/use-cases/change-password.ts
@@ -4,6 +4,7 @@ import { User } from '@/domain/user';
 import { Either, left, right } from '@/shared/either';
 import { InvalidTokenError } from '../errors/invalid-token-error';
 import { UserNotFoundError } from '../errors/user-not-found-error';
+import { BlacklistedTokenRepository } from '../ports/blacklisted-token-repository';
 import { PasswordHasher } from '../ports/password-hasher';
 import { TokenService } from '../ports/token-service';
 import { UserRepository } from '../ports/user-repository';
@@ -22,6 +23,7 @@ export class ChangePassword {
     private readonly tokenService: TokenService,
     private readonly userRepository: UserRepository,
     private readonly passwordHasher: PasswordHasher,
+    private readonly blacklistedTokenRepository: BlacklistedTokenRepository,
   ) {}
 
   async execute(
@@ -36,6 +38,14 @@ export class ChangePassword {
 
     if (errorOrDecodedPayload.isLeft()) {
       return left(errorOrDecodedPayload.value);
+    }
+
+    const isTokenBlacklisted = await this.blacklistedTokenRepository.exists(
+      request.token,
+    );
+
+    if (isTokenBlacklisted) {
+      return left(new InvalidTokenError());
     }
 
     const { userId } = errorOrDecodedPayload.value;
