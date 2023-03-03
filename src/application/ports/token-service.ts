854b454f6dd8e60e3507b003211be7e91207249a
Natã Elienai <nata.elienai@gmail.com>
2023-03-03 20:51:57 -0300

    fix: rewrite token service to generate and decode access and refresh tokens

--- src/application/ports/token-service.ts
+++ src/application/ports/token-service.ts
@@ -7,6 +7,12 @@ type DecodedPayload = {
 };
 
 export interface TokenService {
-  encode(userId: string): Promise<string>;
-  decode(token: string): Promise<Either<InvalidTokenError, DecodedPayload>>;
+  generateAccessToken(userId: string): Promise<string>;
+  generateRefreshToken(userId: string): Promise<string>;
+  decodeAccessToken(
+    token: string,
+  ): Promise<Either<InvalidTokenError, DecodedPayload>>;
+  decodeRefreshToken(
+    token: string,
+  ): Promise<Either<InvalidTokenError, DecodedPayload>>;
 }
