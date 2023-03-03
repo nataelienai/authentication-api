e1b440376d209e612167792007b643b2b89fce2b
Natã Elienai <nata.elienai@gmail.com>
2023-03-03 20:58:22 -0300

    fix: replace token creation with session deletion in sign out use case

--- src/application/use-cases/sign-out.ts
+++ src/application/use-cases/sign-out.ts
@@ -1,37 +1,26 @@
 import { Either, left, right } from '@/shared/either';
 import { InvalidTokenError } from '../errors/invalid-token-error';
 import { SessionRepository } from '../ports/session-repository';
-import { TokenService } from '../ports/token-service';
 
 type SignOutRequest = {
-  token: string;
+  accessToken: string;
 };
 
 export class SignOut {
-  constructor(
-    private readonly tokenService: TokenService,
-    private readonly sessionRepository: SessionRepository,
-  ) {}
+  constructor(private readonly sessionRepository: SessionRepository) {}
 
-  async execute({
-    token,
-  }: SignOutRequest): Promise<Either<InvalidTokenError, void>> {
-    const errorOrDecodedPayload = await this.tokenService.decode(token);
-
-    if (errorOrDecodedPayload.isLeft()) {
-      return left(errorOrDecodedPayload.value);
-    }
-
+  async execute(
+    request: SignOutRequest,
+  ): Promise<Either<InvalidTokenError, void>> {
     const sessionExists = await this.sessionRepository.existsByAccessToken(
-      token,
+      request.accessToken,
     );
 
     if (!sessionExists) {
       return left(new InvalidTokenError());
     }
 
-    const { tokenExpiration } = errorOrDecodedPayload.value;
-    await this.sessionRepository.create({ token, tokenExpiration });
+    await this.sessionRepository.deleteByAccessToken(request.accessToken);
 
     return right(undefined);
   }
