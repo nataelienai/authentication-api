2aa71a752dd08351439504b36b52514b6e820b11
Natã Elienai <nata.elienai@gmail.com>
2023-03-03 21:20:52 -0300

    feat: add refresh access token use case

---
+++ src/application/use-cases/refresh-access-token.ts
@@ -0,0 +1,53 @@
+import { Session } from '@/domain/session';
+import { Either, left, right } from '@/shared/either';
+import { InvalidTokenError } from '../errors/invalid-token-error';
+import { SessionRepository } from '../ports/session-repository';
+import { TokenService } from '../ports/token-service';
+
+type RefreshAccessTokenRequest = {
+  refreshToken: string;
+};
+
+type RefreshAccessTokenResponse = {
+  session: Session;
+};
+
+export class RefreshAccessToken {
+  constructor(
+    private readonly sessionRepository: SessionRepository,
+    private readonly tokenService: TokenService,
+  ) {}
+
+  async execute(
+    request: RefreshAccessTokenRequest,
+  ): Promise<Either<InvalidTokenError, RefreshAccessTokenResponse>> {
+    const session = await this.sessionRepository.findByRefreshToken(
+      request.refreshToken,
+    );
+
+    if (!session) {
+      return left(new InvalidTokenError());
+    }
+
+    const errorOrDecodedPayload = await this.tokenService.decodeRefreshToken(
+      request.refreshToken,
+    );
+
+    if (errorOrDecodedPayload.isLeft()) {
+      return left(errorOrDecodedPayload.value);
+    }
+
+    const { userId } = errorOrDecodedPayload.value;
+    const [accessToken, refreshToken] = await Promise.all([
+      this.tokenService.generateAccessToken(userId),
+      this.tokenService.generateRefreshToken(userId),
+    ]);
+
+    session.accessToken = accessToken;
+    session.refreshToken = refreshToken;
+
+    await this.sessionRepository.update(session);
+
+    return right({ session });
+  }
+}
