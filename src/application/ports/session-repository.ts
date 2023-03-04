10d3076602ef936230b7a1071296fce0a7d40676
Natã Elienai <nata.elienai@gmail.com>
2023-03-03 21:20:40 -0300

    feat: add update and findByRefreshToken to session repository

--- src/application/ports/session-repository.ts
+++ src/application/ports/session-repository.ts
@@ -2,6 +2,8 @@ import { Session } from '@/domain/session';
 
 export interface SessionRepository {
   create(session: Session): Promise<void>;
+  update(session: Session): Promise<void>;
+  findByRefreshToken(refreshToken: string): Promise<Session | null>;
   existsByAccessToken(accessToken: string): Promise<boolean>;
   deleteByAccessToken(accessToken: string): Promise<void>;
 }
