5d186104021ba2eaaa107a84de231b3829fe6e74
Natã Elienai <nata.elienai@gmail.com>
2023-03-03 21:23:22 -0300

    feat: add deleteAllByUserId to session repository

--- src/application/ports/session-repository.ts
+++ src/application/ports/session-repository.ts
@@ -6,4 +6,5 @@ export interface SessionRepository {
   findByRefreshToken(refreshToken: string): Promise<Session | null>;
   existsByAccessToken(accessToken: string): Promise<boolean>;
   deleteByAccessToken(accessToken: string): Promise<void>;
+  deleteAllByUserId(userId: string): Promise<void>;
 }
