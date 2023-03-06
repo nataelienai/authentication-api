61c575f1696ff4ac8d872ba70b8dab98d2b6cf67
Natã Elienai <nata.elienai@gmail.com>
2023-03-06 17:09:51 -0300

    feat: add change password controller

---
+++ src/presentation/controllers/change-password-controller.ts
@@ -0,0 +1,58 @@
+import { UserNotFoundError } from '@/application/errors/user-not-found-error';
+import {
+  ChangePassword,
+  ChangePasswordRequest,
+  ChangePasswordResponse,
+} from '@/application/use-cases/change-password';
+import { Either } from '@/shared/either';
+import { InvalidHttpRequestError } from '../errors/invalid-http-request-error';
+
+type HttpRequest = {
+  body: unknown;
+};
+
+type HttpResponse = {
+  statusCode: number;
+  body: ChangePasswordResponse | Error;
+};
+
+interface HttpRequestValidator {
+  validate(
+    request: HttpRequest,
+  ): Either<InvalidHttpRequestError, ChangePasswordRequest>;
+}
+
+export class ChangePasswordController {
+  constructor(
+    private readonly changePassword: ChangePassword,
+    private readonly httpRequestValidator: HttpRequestValidator,
+  ) {}
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    const errorOrChangePasswordRequest =
+      this.httpRequestValidator.validate(request);
+
+    if (errorOrChangePasswordRequest.isLeft()) {
+      const error = errorOrChangePasswordRequest.value;
+      return { statusCode: 400, body: error };
+    }
+
+    const changePasswordRequest = errorOrChangePasswordRequest.value;
+    const errorOrChangePasswordResponse = await this.changePassword.execute(
+      changePasswordRequest,
+    );
+
+    if (errorOrChangePasswordResponse.isLeft()) {
+      const error = errorOrChangePasswordResponse.value;
+
+      if (error instanceof UserNotFoundError) {
+        return { statusCode: 404, body: error };
+      }
+
+      return { statusCode: 400, body: error };
+    }
+
+    const changePasswordResponse = errorOrChangePasswordResponse.value;
+    return { statusCode: 200, body: changePasswordResponse };
+  }
+}
