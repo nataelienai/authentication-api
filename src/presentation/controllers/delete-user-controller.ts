af547f00a06e7f8f9ab3cdd6b89b6e3a690b5825
Natã Elienai <nata.elienai@gmail.com>
2023-03-06 17:18:19 -0300

    feat: add delete user controller

---
+++ src/presentation/controllers/delete-user-controller.ts
@@ -0,0 +1,54 @@
+import { UserNotFoundError } from '@/application/errors/user-not-found-error';
+import {
+  DeleteUser,
+  DeleteUserRequest,
+} from '@/application/use-cases/delete-user';
+import { Either } from '@/shared/either';
+import { InvalidHttpRequestError } from '../errors/invalid-http-request-error';
+
+type HttpRequest = {
+  body: unknown;
+};
+
+type HttpResponse = {
+  statusCode: number;
+  body?: Error;
+};
+
+interface HttpRequestValidator {
+  validate(
+    request: HttpRequest,
+  ): Either<InvalidHttpRequestError, DeleteUserRequest>;
+}
+
+export class DeleteUserController {
+  constructor(
+    private readonly deleteUser: DeleteUser,
+    private readonly httpRequestValidator: HttpRequestValidator,
+  ) {}
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    const errorOrDeleteUserRequest =
+      this.httpRequestValidator.validate(request);
+
+    if (errorOrDeleteUserRequest.isLeft()) {
+      const error = errorOrDeleteUserRequest.value;
+      return { statusCode: 400, body: error };
+    }
+
+    const deleteUserRequest = errorOrDeleteUserRequest.value;
+    const errorOrVoid = await this.deleteUser.execute(deleteUserRequest);
+
+    if (errorOrVoid.isLeft()) {
+      const error = errorOrVoid.value;
+
+      if (error instanceof UserNotFoundError) {
+        return { statusCode: 404, body: error };
+      }
+
+      return { statusCode: 400, body: error };
+    }
+
+    return { statusCode: 204 };
+  }
+}
