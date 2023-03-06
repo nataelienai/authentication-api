f8f10e1ec955fc7a6a25acd32d5ca6a57e190c0b
Natã Elienai <nata.elienai@gmail.com>
2023-03-06 14:57:46 -0300

    feat: add sign in controller

---
+++ src/presentation/controllers/sign-in-controller.ts
@@ -0,0 +1,55 @@
+import { UserNotFoundError } from '@/application/errors/user-not-found-error';
+import {
+  SignIn,
+  SignInRequest,
+  SignInResponse,
+} from '@/application/use-cases/sign-in';
+import { Either } from '@/shared/either';
+import { InvalidHttpRequestError } from '../errors/invalid-http-request-error';
+
+type HttpRequest = {
+  body: unknown;
+};
+
+type HttpResponse = {
+  statusCode: number;
+  body: SignInResponse | Error;
+};
+
+interface HttpRequestValidator {
+  validate(
+    request: HttpRequest,
+  ): Either<InvalidHttpRequestError, SignInRequest>;
+}
+
+export class SignInController {
+  constructor(
+    private readonly signIn: SignIn,
+    private readonly httpRequestValidator: HttpRequestValidator,
+  ) {}
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    const errorOrSignInRequest = this.httpRequestValidator.validate(request);
+
+    if (errorOrSignInRequest.isLeft()) {
+      const error = errorOrSignInRequest.value;
+      return { statusCode: 400, body: error };
+    }
+
+    const signInRequest = errorOrSignInRequest.value;
+    const errorOrSignInResponse = await this.signIn.execute(signInRequest);
+
+    if (errorOrSignInResponse.isLeft()) {
+      const error = errorOrSignInResponse.value;
+
+      if (error instanceof UserNotFoundError) {
+        return { statusCode: 404, body: error };
+      }
+
+      return { statusCode: 400, body: error };
+    }
+
+    const signInResponse = errorOrSignInResponse.value;
+    return { statusCode: 200, body: signInResponse };
+  }
+}
