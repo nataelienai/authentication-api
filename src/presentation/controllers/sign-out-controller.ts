54a210843c76293dce51d36e960ce553808f9508
Natã Elienai <nata.elienai@gmail.com>
2023-03-07 19:52:20 -0300

    refactor: make sign out controller extend controller

--- src/presentation/controllers/sign-out-controller.ts
+++ src/presentation/controllers/sign-out-controller.ts
@@ -1,23 +1,19 @@
 import { SignOut, SignOutRequest } from '@/application/use-cases/sign-out';
-import { HttpRequest } from '../ports/http-request';
 import { HttpRequestValidator } from '../ports/http-request-validator';
 import { HttpResponse } from '../ports/http-response';
+import { Controller } from './controller';
 
-export class SignOutController {
+export class SignOutController extends Controller<SignOutRequest, void> {
   constructor(
     private readonly signOut: SignOut,
-    private readonly httpRequestValidator: HttpRequestValidator<SignOutRequest>,
-  ) {}
+    httpRequestValidator: HttpRequestValidator<SignOutRequest>,
+  ) {
+    super(httpRequestValidator);
+  }
 
-  async handle(request: HttpRequest): Promise<HttpResponse<Error | void>> {
-    const errorOrSignOutRequest = this.httpRequestValidator.validate(request);
-
-    if (errorOrSignOutRequest.isLeft()) {
-      const error = errorOrSignOutRequest.value;
-      return { statusCode: 400, body: error };
-    }
-
-    const signOutRequest = errorOrSignOutRequest.value;
+  async execute(
+    signOutRequest: SignOutRequest,
+  ): Promise<HttpResponse<Error | void>> {
     const errorOrVoid = await this.signOut.execute(signOutRequest);
 
     if (errorOrVoid.isLeft()) {
