e687ed55679967cbcf7c9ef7139334b8d1dda3f2
Natã Elienai <nata.elienai@gmail.com>
2023-03-07 19:53:32 -0300

    refactor: make sign up controller extend controller

--- src/presentation/controllers/sign-up-controller.ts
+++ src/presentation/controllers/sign-up-controller.ts
@@ -4,27 +4,24 @@ import {
   SignUpRequest,
   SignUpResponse,
 } from '@/application/use-cases/sign-up';
-import { HttpRequest } from '../ports/http-request';
 import { HttpRequestValidator } from '../ports/http-request-validator';
 import { HttpResponse } from '../ports/http-response';
+import { Controller } from './controller';
 
-export class SignUpController {
+export class SignUpController extends Controller<
+  SignUpRequest,
+  SignUpResponse
+> {
   constructor(
     private readonly signUp: SignUp,
-    private readonly httpRequestValidator: HttpRequestValidator<SignUpRequest>,
-  ) {}
+    httpRequestValidator: HttpRequestValidator<SignUpRequest>,
+  ) {
+    super(httpRequestValidator);
+  }
 
-  async handle(
-    request: HttpRequest,
+  async execute(
+    signUpRequest: SignUpRequest,
   ): Promise<HttpResponse<Error | SignUpResponse>> {
-    const errorOrSignUpRequest = this.httpRequestValidator.validate(request);
-
-    if (errorOrSignUpRequest.isLeft()) {
-      const error = errorOrSignUpRequest.value;
-      return { statusCode: 400, body: error };
-    }
-
-    const signUpRequest = errorOrSignUpRequest.value;
     const errorOrSignUpResponse = await this.signUp.execute(signUpRequest);
 
     if (errorOrSignUpResponse.isLeft()) {
