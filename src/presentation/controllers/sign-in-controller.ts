2e65f43415195fc45513f5eb7088295e36068060
Natã Elienai <nata.elienai@gmail.com>
2023-03-07 19:49:59 -0300

    refactor: make sign in controller extend controller

--- src/presentation/controllers/sign-in-controller.ts
+++ src/presentation/controllers/sign-in-controller.ts
@@ -4,27 +4,24 @@ import {
   SignInRequest,
   SignInResponse,
 } from '@/application/use-cases/sign-in';
-import { HttpRequest } from '../ports/http-request';
 import { HttpRequestValidator } from '../ports/http-request-validator';
 import { HttpResponse } from '../ports/http-response';
+import { Controller } from './controller';
 
-export class SignInController {
+export class SignInController extends Controller<
+  SignInRequest,
+  SignInResponse
+> {
   constructor(
     private readonly signIn: SignIn,
-    private readonly httpRequestValidator: HttpRequestValidator<SignInRequest>,
-  ) {}
+    httpRequestValidator: HttpRequestValidator<SignInRequest>,
+  ) {
+    super(httpRequestValidator);
+  }
 
-  async handle(
-    request: HttpRequest,
+  async execute(
+    signInRequest: SignInRequest,
   ): Promise<HttpResponse<Error | SignInResponse>> {
-    const errorOrSignInRequest = this.httpRequestValidator.validate(request);
-
-    if (errorOrSignInRequest.isLeft()) {
-      const error = errorOrSignInRequest.value;
-      return { statusCode: 400, body: error };
-    }
-
-    const signInRequest = errorOrSignInRequest.value;
     const errorOrSignInResponse = await this.signIn.execute(signInRequest);
 
     if (errorOrSignInResponse.isLeft()) {
