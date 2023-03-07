cbe6a4f67cb4e46f6c66921f25b0a7af4a7827ed
Natã Elienai <nata.elienai@gmail.com>
2023-03-07 19:37:43 -0300

    refactor: make delete user controller extend controller

--- src/presentation/controllers/delete-user-controller.ts
+++ src/presentation/controllers/delete-user-controller.ts
@@ -3,26 +3,21 @@ import {
   DeleteUser,
   DeleteUserRequest,
 } from '@/application/use-cases/delete-user';
-import { HttpRequest } from '../ports/http-request';
 import { HttpRequestValidator } from '../ports/http-request-validator';
 import { HttpResponse } from '../ports/http-response';
+import { Controller } from './controller';
 
-export class DeleteUserController {
+export class DeleteUserController extends Controller<DeleteUserRequest, void> {
   constructor(
     private readonly deleteUser: DeleteUser,
-    private readonly httpRequestValidator: HttpRequestValidator<DeleteUserRequest>,
-  ) {}
+    httpRequestValidator: HttpRequestValidator<DeleteUserRequest>,
+  ) {
+    super(httpRequestValidator);
+  }
 
-  async handle(request: HttpRequest): Promise<HttpResponse<Error | void>> {
-    const errorOrDeleteUserRequest =
-      this.httpRequestValidator.validate(request);
-
-    if (errorOrDeleteUserRequest.isLeft()) {
-      const error = errorOrDeleteUserRequest.value;
-      return { statusCode: 400, body: error };
-    }
-
-    const deleteUserRequest = errorOrDeleteUserRequest.value;
+  async execute(
+    deleteUserRequest: DeleteUserRequest,
+  ): Promise<HttpResponse<Error | void>> {
     const errorOrVoid = await this.deleteUser.execute(deleteUserRequest);
 
     if (errorOrVoid.isLeft()) {
