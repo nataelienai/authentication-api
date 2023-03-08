0c96088a7f8b912ba84c704d14adf749f4790fae
Natã Elienai <nata.elienai@gmail.com>
2023-03-08 17:57:00 -0300

    feat: add route registration to sign out controller

--- src/presentation/controllers/sign-out-controller.ts
+++ src/presentation/controllers/sign-out-controller.ts
@@ -1,14 +1,17 @@
 import { SignOut, SignOutRequest } from '@/application/use-cases/sign-out';
 import { HttpRequestValidator } from '../ports/http-request-validator';
 import { HttpResponse } from '../ports/http-response';
+import { HttpServer } from '../ports/http-server';
 import { Controller } from './controller';
 
 export class SignOutController extends Controller<SignOutRequest, void> {
   constructor(
     private readonly signOut: SignOut,
     httpRequestValidator: HttpRequestValidator<SignOutRequest>,
+    httpServer: HttpServer,
   ) {
     super(httpRequestValidator);
+    httpServer.on('post', '/sign-out', this);
   }
 
   protected async execute(
