702c09ea3c91fb9892bb773834a46ac3f0f49b2d
Natã Elienai <nata.elienai@gmail.com>
2023-03-08 16:30:38 -0300

    feat: create the method type for http server

--- src/presentation/ports/http-server.ts
+++ src/presentation/ports/http-server.ts
@@ -1,6 +1,8 @@
 import { HttpController } from './http-controller';
 
+type Method = 'get' | 'post' | 'put' | 'patch' | 'delete';
+
 export interface HttpServer {
-  on(method: string, path: string, controller: HttpController): void;
+  on(method: Method, path: string, controller: HttpController): void;
   listen(port: number): void;
 }
