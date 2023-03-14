998aa53300a24177cd55e91804a15a5b8dc746c6
Natã Elienai <nata.elienai@gmail.com>
2023-03-14 16:44:06 -0300

    refactor: make email regexp a static property of email entity

--- src/domain/email.ts
+++ src/domain/email.ts
@@ -1,11 +1,11 @@
 import { Either, left, right } from '@/shared/either';
 import { InvalidEmailError } from './errors/invalid-email-error';
 
-// regexp source: https://github.com/angular/angular/blob/15.1.x/packages/forms/src/validators.ts
-const EMAIL_REGEXP =
-  /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-
 export class Email {
+  // regexp source: https://github.com/angular/angular/blob/15.1.x/packages/forms/src/validators.ts
+  private static readonly FORMAT_REGEXP =
+    /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
   private constructor(private readonly email: string) {}
 
   get value() {
@@ -21,6 +21,6 @@ export class Email {
   }
 
   static hasValidFormat(email: string) {
-    return email.match(EMAIL_REGEXP);
+    return email.match(Email.FORMAT_REGEXP);
   }
 }
