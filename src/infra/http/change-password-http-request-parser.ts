8b67420d976847512f5bac4bc6f3f44d7069ebfa
Natã Elienai <nata.elienai@gmail.com>
2023-03-14 22:35:35 -0300

    feat: add change password http request parser

---
+++ src/infra/http/change-password-http-request-parser.ts
@@ -0,0 +1,27 @@
+import { ChangePasswordRequest } from '@/application/use-cases/change-password';
+import { HttpRequest } from '@/presentation/ports/http-request';
+import { z } from 'zod';
+import { ZodHttpRequestParser } from './zod-http-request-parser';
+
+export class ChangePasswordHttpRequestParser extends ZodHttpRequestParser<ChangePasswordRequest> {
+  // eslint-disable-next-line class-methods-use-this
+  protected parseRequest(request: HttpRequest): ChangePasswordRequest {
+    const schema = z.object({
+      headers: z.object({
+        accessToken: z
+          .string()
+          .startsWith('Bearer ')
+          .transform((token) => token.split('Bearer ')[1]),
+      }),
+      body: z.object({
+        password: z.string(),
+      }),
+    });
+    const parsedRequest = schema.parse(request);
+
+    return {
+      password: parsedRequest.body.password,
+      accessToken: parsedRequest.headers.accessToken,
+    };
+  }
+}
