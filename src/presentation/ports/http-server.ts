94aa56524da37c8099756768ebad6aa0c84b99af
Natã Elienai <nata.elienai@gmail.com>
2023-03-15 00:58:28 -0300

    fix: export method type from http server

--- src/presentation/ports/http-server.ts
+++ src/presentation/ports/http-server.ts
@@ -1,6 +1,6 @@
 import { HttpController } from './http-controller';
 
-type Method = 'get' | 'post' | 'put' | 'patch' | 'delete';
+export type Method = 'get' | 'post' | 'put' | 'patch' | 'delete';
 
 export interface HttpServer {
   on(method: Method, path: string, controller: HttpController): void;
