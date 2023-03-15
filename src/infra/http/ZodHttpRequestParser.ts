4aecef66985f4387b0793a9610aa13328c702a41
Natã Elienai <nata.elienai@gmail.com>
2023-03-14 21:37:52 -0300

    feat: create zod http request parser

---
+++ src/infra/http/ZodHttpRequestParser.ts
@@ -0,0 +1,21 @@
+import { InvalidHttpRequestError } from '@/presentation/errors/invalid-http-request-error';
+import { HttpRequest } from '@/presentation/ports/http-request';
+import { HttpRequestParser } from '@/presentation/ports/http-request-parser';
+import { Either, left, right } from '@/shared/either';
+import { ZodError } from 'zod';
+
+export abstract class ZodHttpRequestParser<T> implements HttpRequestParser<T> {
+  parse(request: HttpRequest): Either<InvalidHttpRequestError, T> {
+    try {
+      const useCaseRequest = this.parseRequest(request);
+      return right(useCaseRequest);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return left(new InvalidHttpRequestError(error.message));
+      }
+      throw error;
+    }
+  }
+
+  protected abstract parseRequest(request: HttpRequest): T;
+}
