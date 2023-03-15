7bb255dc697853c70aefc6e70d2fa17bc8bc4342
Natã Elienai <nata.elienai@gmail.com>
2023-03-14 21:38:04 -0300

    refactor: make sign up http request parser extend zod http request parser

--- src/infra/http/SignUpHttpRequestParser.ts
+++ src/infra/http/SignUpHttpRequestParser.ts
@@ -1,28 +1,16 @@
 import { SignUpRequest } from '@/application/use-cases/sign-up';
-import { InvalidHttpRequestError } from '@/presentation/errors/invalid-http-request-error';
 import { HttpRequest } from '@/presentation/ports/http-request';
-import { HttpRequestParser } from '@/presentation/ports/http-request-parser';
-import { Either, left, right } from '@/shared/either';
-import { z, ZodError } from 'zod';
+import { z } from 'zod';
+import { ZodHttpRequestParser } from './ZodHttpRequestParser';
 
-export class SignUpHttpRequestParser
-  implements HttpRequestParser<SignUpRequest>
-{
-  private static readonly schema = z.object({
-    email: z.string(),
-    password: z.string(),
-  });
-
+export class SignUpHttpRequestParser extends ZodHttpRequestParser<SignUpRequest> {
   // eslint-disable-next-line class-methods-use-this
-  parse(request: HttpRequest): Either<InvalidHttpRequestError, SignUpRequest> {
-    try {
-      const signUpRequest = SignUpHttpRequestParser.schema.parse(request.body);
-      return right(signUpRequest);
-    } catch (error) {
-      if (error instanceof ZodError) {
-        return left(new InvalidHttpRequestError(error.message));
-      }
-      throw error;
-    }
+  protected parseRequest(request: HttpRequest): SignUpRequest {
+    const schema = z.object({
+      email: z.string(),
+      password: z.string(),
+    });
+
+    return schema.parse(request.body);
   }
 }
