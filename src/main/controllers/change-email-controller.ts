a2786b009c96b82e959147f73ed87569e1506f9e
Natã Elienai <nata.elienai@gmail.com>
2023-03-16 21:36:25 -0300

    feat: add change email controller loading

---
+++ src/main/controllers/change-email-controller.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-new */
+import { ChangeEmail } from '@/application/use-cases/change-email';
+import { ChangeEmailHttpRequestParser } from '@/infra/http/parsers/change-email-http-request-parser';
+import { ChangeEmailController } from '@/presentation/controllers/change-email-controller';
+import { getSessionRepository } from '../repositories/session-repository';
+import { getUserRepository } from '../repositories/user-repository';
+import { getHttpServer } from '../servers/http-server';
+import { getTokenService } from '../services/token-service';
+
+export async function loadChangeEmailController() {
+  const changeEmail = new ChangeEmail(
+    getTokenService(),
+    getUserRepository(),
+    await getSessionRepository(),
+  );
+
+  const changeEmailHttpRequestParser = new ChangeEmailHttpRequestParser();
+
+  new ChangeEmailController(
+    changeEmail,
+    changeEmailHttpRequestParser,
+    getHttpServer(),
+  );
+}
