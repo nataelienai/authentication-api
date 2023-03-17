949f6303c2f74d924d8b5d85f6dd8314f32b8cec
Natã Elienai <nata.elienai@gmail.com>
2023-03-17 16:22:07 -0300

    refactor: rename main layer controllers folder to loaders

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
-import { loadSignUpController } from './controllers/sign-up-controller';
-import { loadSignInController } from './controllers/sign-in-controller';
-import { loadSignOutController } from './controllers/sign-out-controller';
-import { loadRefreshAccessTokenController } from './controllers/refresh-access-token-controller';
-import { loadGetUserController } from './controllers/get-user-controller';
-import { loadChangeEmailController } from './controllers/change-email-controller';
-import { loadChangePasswordController } from './controllers/change-password-controller';
-import { loadDeleteUserController } from './controllers/delete-user-controller';
+import { loadSignUpController } from './loaders/sign-up-controller';
+import { loadSignInController } from './loaders/sign-in-controller';
+import { loadSignOutController } from './loaders/sign-out-controller';
+import { loadRefreshAccessTokenController } from './loaders/refresh-access-token-controller';
+import { loadGetUserController } from './loaders/get-user-controller';
+import { loadChangeEmailController } from './loaders/change-email-controller';
+import { loadChangePasswordController } from './loaders/change-password-controller';
+import { loadDeleteUserController } from './loaders/delete-user-controller';
 import { getHttpServer } from './servers/http-server';
 
 async function run() {
