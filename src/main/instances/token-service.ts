d8c1ca2cb79c3c7de4dac9c95480ca1ff57adb00
Natã Elienai <nata.elienai@gmail.com>
2023-03-18 18:15:52 -0300

    refactor: change token service instantiation to use validated env variables

--- src/main/instances/token-service.ts
+++ src/main/instances/token-service.ts
@@ -1,13 +1,15 @@
 import { TokenService } from '@/application/ports/token-service';
 import { JwtTokenService } from '@/infra/services/jwt-token-service';
+import { env } from '../env';
 
 let tokenService: TokenService;
 
 export function getTokenService() {
   if (!tokenService) {
-    const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET ?? '';
-    const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET ?? '';
-    tokenService = new JwtTokenService(accessTokenSecret, refreshTokenSecret);
+    tokenService = new JwtTokenService(
+      env.ACCESS_TOKEN_SECRET,
+      env.REFRESH_TOKEN_SECRET,
+    );
   }
   return tokenService;
 }
