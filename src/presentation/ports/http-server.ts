ff3b236d6c0ccb0a19327e4378d6740a9a3c2a2b
Natã Elienai <nata.elienai@gmail.com>
2023-03-18 13:20:27 -0300

    refactor: remove not used method type

--- src/presentation/ports/http-server.ts
+++ src/presentation/ports/http-server.ts
@@ -1,7 +1,5 @@
 import { HttpController } from './http-controller';
 
-export type Method = 'get' | 'post' | 'put' | 'patch' | 'delete';
-
 export interface HttpServer {
   register(controller: HttpController): void;
   listen(port: number): void;
