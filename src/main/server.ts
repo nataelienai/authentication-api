355bec1f52d23d7c45738355e578637065741965
Natã Elienai <nata.elienai@gmail.com>
2023-03-18 13:49:38 -0300

    refactor: rename singletons folder to instances

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
-import { getSignUpController } from './singletons/sign-up-controller';
-import { getSignInController } from './singletons/sign-in-controller';
-import { getSignOutController } from './singletons/sign-out-controller';
-import { getRefreshAccessTokenController } from './singletons/refresh-access-token-controller';
-import { getGetUserController } from './singletons/get-user-controller';
-import { getChangeEmailController } from './singletons/change-email-controller';
-import { getChangePasswordController } from './singletons/change-password-controller';
-import { getDeleteUserController } from './singletons/delete-user-controller';
-import { getHttpServer } from './singletons/http-server';
+import { getSignUpController } from './instances/sign-up-controller';
+import { getSignInController } from './instances/sign-in-controller';
+import { getSignOutController } from './instances/sign-out-controller';
+import { getRefreshAccessTokenController } from './instances/refresh-access-token-controller';
+import { getGetUserController } from './instances/get-user-controller';
+import { getChangeEmailController } from './instances/change-email-controller';
+import { getChangePasswordController } from './instances/change-password-controller';
+import { getDeleteUserController } from './instances/delete-user-controller';
+import { getHttpServer } from './instances/http-server';
 
 async function run() {
   const controllers = await Promise.all([
