eee36b3a11314925b9510951d3c9a2e11ffdb103
Natã Elienai <nata.elienai@gmail.com>
2023-03-20 21:17:18 -0300

    refactor: change refresh access token use case to use auth class

--- src/application/use-cases/refresh-access-token.ts
+++ src/application/use-cases/refresh-access-token.ts
@@ -1,8 +1,7 @@
 import { Session } from '@/domain/session';
 import { Either, left, right } from '@/shared/either';
 import { InvalidTokenError } from '../errors/invalid-token-error';
-import { SessionRepository } from '../ports/session-repository';
-import { TokenService } from '../ports/token-service';
+import { Auth } from './auth';
 
 export type RefreshAccessTokenRequest = {
   refreshToken: string;
@@ -13,38 +12,20 @@ export type RefreshAccessTokenResponse = {
 };
 
 export class RefreshAccessToken {
-  constructor(
-    private readonly sessionRepository: SessionRepository,
-    private readonly tokenService: TokenService,
-  ) {}
+  constructor(private readonly auth: Auth) {}
 
   async execute(
     request: RefreshAccessTokenRequest,
   ): Promise<Either<InvalidTokenError, RefreshAccessTokenResponse>> {
-    const [errorOrDecodedPayload, session] = await Promise.all([
-      this.tokenService.decodeRefreshToken(request.refreshToken),
-      this.sessionRepository.findByRefreshToken(request.refreshToken),
-    ]);
+    const errorOrSession = await this.auth.refreshAccessToken(
+      request.refreshToken,
+    );
 
-    if (errorOrDecodedPayload.isLeft()) {
-      return left(errorOrDecodedPayload.value);
+    if (errorOrSession.isLeft()) {
+      return left(errorOrSession.value);
     }
 
-    if (!session) {
-      return left(new InvalidTokenError());
-    }
-
-    const { userId } = errorOrDecodedPayload.value;
-    const [accessToken, refreshToken] = await Promise.all([
-      this.tokenService.generateAccessToken(userId),
-      this.tokenService.generateRefreshToken(userId),
-    ]);
-
-    session.accessToken = accessToken;
-    session.refreshToken = refreshToken;
-
-    await this.sessionRepository.update(session);
-
+    const session = errorOrSession.value;
     return right({ session });
   }
 }
--- src/main/instances/refresh-access-token-controller.ts
+++ src/main/instances/refresh-access-token-controller.ts
@@ -1,14 +1,10 @@
 import { RefreshAccessToken } from '@/application/use-cases/refresh-access-token';
 import { RefreshAccessTokenHttpRequestParser } from '@/infra/http/parsers/refresh-access-token-http-request-parser';
 import { RefreshAccessTokenController } from '@/presentation/controllers/refresh-access-token-controller';
-import { getSessionRepository } from './session-repository';
-import { getTokenService } from './token-service';
+import { getAuth } from './auth';
 
 export async function getRefreshAccessTokenController() {
-  const refreshAccessToken = new RefreshAccessToken(
-    await getSessionRepository(),
-    getTokenService(),
-  );
+  const refreshAccessToken = new RefreshAccessToken(await getAuth());
 
   const refreshAccessTokenHttpRequestParser =
     new RefreshAccessTokenHttpRequestParser();
