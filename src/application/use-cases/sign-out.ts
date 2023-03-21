6610da7fb1a4a7df0475d5446973bcb412183486
Natã Elienai <nata.elienai@gmail.com>
2023-03-20 21:16:58 -0300

    refactor: change sign out use case to use auth class

--- src/application/use-cases/sign-out.ts
+++ src/application/use-cases/sign-out.ts
@@ -1,32 +1,17 @@
-import { Either, left, right } from '@/shared/either';
+import { Either } from '@/shared/either';
 import { InvalidTokenError } from '../errors/invalid-token-error';
-import { SessionRepository } from '../ports/session-repository';
-import { TokenService } from '../ports/token-service';
+import { Auth } from './auth';
 
 export type SignOutRequest = {
   accessToken: string;
 };
 
 export class SignOut {
-  constructor(
-    private readonly tokenService: TokenService,
-    private readonly sessionRepository: SessionRepository,
-  ) {}
+  constructor(private readonly auth: Auth) {}
 
   async execute(
     request: SignOutRequest,
   ): Promise<Either<InvalidTokenError, void>> {
-    const [isTokenValid, sessionExists] = await Promise.all([
-      this.tokenService.isAccessTokenValid(request.accessToken),
-      this.sessionRepository.existsByAccessToken(request.accessToken),
-    ]);
-
-    if (!isTokenValid || !sessionExists) {
-      return left(new InvalidTokenError());
-    }
-
-    await this.sessionRepository.deleteByAccessToken(request.accessToken);
-
-    return right(undefined);
+    return this.auth.revokeAccessToken(request.accessToken);
   }
 }
--- src/main/instances/sign-out-controller.ts
+++ src/main/instances/sign-out-controller.ts
@@ -1,11 +1,10 @@
 import { SignOut } from '@/application/use-cases/sign-out';
 import { SignOutHttpRequestParser } from '@/infra/http/parsers/sign-out-http-request-parser';
 import { SignOutController } from '@/presentation/controllers/sign-out-controller';
-import { getSessionRepository } from './session-repository';
-import { getTokenService } from './token-service';
+import { getAuth } from './auth';
 
 export async function getSignOutController() {
-  const signOut = new SignOut(getTokenService(), await getSessionRepository());
+  const signOut = new SignOut(await getAuth());
 
   const signOutHttpRequestParser = new SignOutHttpRequestParser();
 
