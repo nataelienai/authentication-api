76344eeeb1e675dedb4f767555796c274c1c6bba
Natã Elienai <nata.elienai@gmail.com>
2023-03-27 17:42:22 -0300

    feat: add winston logger implementation

---
+++ src/infra/logging/winston-logger.ts
@@ -0,0 +1,31 @@
+import winston, { createLogger, format, transports } from 'winston';
+import { Logger } from './logger';
+
+export class WinstonLogger implements Logger {
+  private readonly logger: winston.Logger;
+
+  constructor() {
+    const logFormat = format.printf(
+      ({ level, message, stack, timestamp }) =>
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        `${timestamp} ${level}: ${stack ?? message}`,
+    );
+
+    this.logger = createLogger({
+      level: 'info',
+      format: format.combine(format.timestamp(), logFormat),
+      transports: [new transports.Console()],
+    });
+  }
+
+  info(message: string): void {
+    this.logger.info(message);
+  }
+
+  error(message: string): void;
+  error(error: Error): void;
+  error(reason: unknown): void;
+  error(messageOrErrorOrReason: unknown): void {
+    this.logger.error(messageOrErrorOrReason);
+  }
+}
