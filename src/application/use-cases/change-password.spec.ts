d58d14f8d513aefa921ffa18c34b34dc5a5f9ff6
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-06-26 20:31:19 -0300

    test: add tests for the change password use case

---
+++ src/application/use-cases/change-password.spec.ts
@@ -0,0 +1,135 @@
+import { FakePasswordHasher } from '@test/doubles/fake-password-hasher';
+import { FakeTokenService } from '@test/doubles/fake-token-service';
+import { InMemorySessionRepository } from '@test/doubles/in-memory-session-repository';
+import { InMemoryUserRepository } from '@test/doubles/in-memory-user-repository';
+import { Email } from '@/domain/email';
+import { Password } from '@/domain/password';
+import { Session } from '@/domain/session';
+import { User } from '@/domain/user';
+import { InvalidPasswordError } from '@/domain/errors/invalid-password-error';
+import { InvalidTokenError } from '../errors/invalid-token-error';
+import { UserNotFoundError } from '../errors/user-not-found-error';
+import { PasswordHasher } from '../ports/password-hasher';
+import { SessionRepository } from '../ports/session-repository';
+import { TokenService } from '../ports/token-service';
+import { UserRepository } from '../ports/user-repository';
+import { Auth } from './auth';
+import { ChangePassword, ChangePasswordResponse } from './change-password';
+
+describe('Change Password', () => {
+  let passwordHasher: PasswordHasher;
+  let userRepository: UserRepository;
+  let tokenService: TokenService;
+  let sessionRepository: SessionRepository;
+  let auth: Auth;
+  let changePassword: ChangePassword;
+
+  beforeEach(() => {
+    passwordHasher = new FakePasswordHasher();
+    userRepository = new InMemoryUserRepository();
+    tokenService = new FakeTokenService();
+    sessionRepository = new InMemorySessionRepository();
+    auth = new Auth(tokenService, sessionRepository);
+    changePassword = new ChangePassword(userRepository, passwordHasher, auth);
+  });
+
+  test('When access token is invalid, return an error', async () => {
+    // Arrange
+    // Act
+    const errorOrResponse = await changePassword.execute({
+      accessToken: 'invalid_token',
+      password: 'new_password',
+    });
+
+    // Assert
+    expect(errorOrResponse.isLeft()).toBe(true);
+    expect(errorOrResponse.value).toBeInstanceOf(InvalidTokenError);
+  });
+
+  test('When user is not found, return an error', async () => {
+    // Arrange
+    const userId = 'fake_id';
+    const accessToken = await tokenService.generateAccessToken(userId);
+    const refreshToken = await tokenService.generateRefreshToken(userId);
+    const session = Session.create({ accessToken, refreshToken, userId });
+    await sessionRepository.create(session);
+
+    // Act
+    const errorOrResponse = await changePassword.execute({
+      accessToken,
+      password: 'new_password',
+    });
+
+    // Assert
+    expect(errorOrResponse.isLeft()).toBe(true);
+    expect(errorOrResponse.value).toBeInstanceOf(UserNotFoundError);
+  });
+
+  test('When password is invalid, return an error', async () => {
+    // Arrange
+    const user = User.create({
+      email: Email.create('valid@email.com').value as Email,
+      hashedPassword: await passwordHasher.hash(
+        Password.create('password123').value as Password,
+      ),
+    }).value as User;
+    await userRepository.create(user);
+
+    const accessToken = await tokenService.generateAccessToken(user.id);
+    const refreshToken = await tokenService.generateRefreshToken(user.id);
+    const session = Session.create({
+      accessToken,
+      refreshToken,
+      userId: user.id,
+    });
+    await sessionRepository.create(session);
+
+    // Act
+    const errorOrResponse = await changePassword.execute({
+      accessToken,
+      password: 'short',
+    });
+
+    // Assert
+    expect(errorOrResponse.isLeft()).toBe(true);
+    expect(errorOrResponse.value).toBeInstanceOf(InvalidPasswordError);
+  });
+
+  test('When email and token are valid and user is found, return the user new info', async () => {
+    // Arrange
+    const user = User.create({
+      email: Email.create('valid@email.com').value as Email,
+      hashedPassword: await passwordHasher.hash(
+        Password.create('password123').value as Password,
+      ),
+    }).value as User;
+    await userRepository.create(user);
+
+    const accessToken = await tokenService.generateAccessToken(user.id);
+    const refreshToken = await tokenService.generateRefreshToken(user.id);
+    const session = Session.create({
+      accessToken,
+      refreshToken,
+      userId: user.id,
+    });
+    await sessionRepository.create(session);
+
+    const newPassword = 'new_password';
+
+    // Act
+    const errorOrResponse = await changePassword.execute({
+      accessToken,
+      password: newPassword,
+    });
+
+    // Assert
+    expect(errorOrResponse.isRight()).toBe(true);
+
+    const response = errorOrResponse.value as ChangePasswordResponse;
+    const passwordChanged = await passwordHasher.compare(
+      newPassword,
+      response.user.hashedPassword,
+    );
+    expect(passwordChanged).toBe(true);
+  });
+});
