b344db4f6fbbac1bb67189b391e708fe4e580f8a
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-06-29 19:01:33 -0300

    feat: use REDIS_URL env variable to connect to redis

--- src/main/env.ts
+++ src/main/env.ts
@@ -7,6 +7,7 @@ const envSchema = z.object({
   ACCESS_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_SECRET: z.string(),
   PORT: z.coerce.number().default(DEFAULT_PORT),
+  REDIS_URL: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
--- src/main/instances/redis.ts
+++ src/main/instances/redis.ts
@@ -1,11 +1,12 @@
 import { createClient, RedisClientType } from 'redis';
 import { getLogger } from './logger';
+import { env } from '../env';
 
 let redisClient: RedisClientType;
 
 export async function getRedisClient() {
   if (!redisClient) {
-    redisClient = createClient();
+    redisClient = createClient({ url: env.REDIS_URL });
     await redisClient.connect();
 
     const logger = getLogger();
